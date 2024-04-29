b35c9f66e2ce77877f59c5ecf75061d2202e903b
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-04-29 09:25:51 +0100

    Added requirement.txt file.

--- producer.py
+++ producer.py
@@ -3,18 +3,19 @@ from data import *
 import json
 import time
 
-def json_serializer(data): 
-    return json.dumps(data).encode('utf-8')
+# def json_serializer(data): 
+#     return json.dumps(data).encode('utf-8')
 
 producer = KafkaProducer(
     bootstrap_servers = ['localhost:9092'],
-    value_serializer = json_serializer
+    value_serializer = lambda x: json.dumps(x).encode('utf-8'),
+    partitioner = lambda y,z,a: 0  #This line helps us to send message to a specific partition.
 )
 
 
 if __name__ == '__main__':
-    while 1 == 1:
+    while True:
         registered_user = get_registered_user()
         print(registered_user)
-        producer.send('report', registered_user)
-        time.sleep(6)
+        producer.send('report1', registered_user)
+        time.sleep(20)
